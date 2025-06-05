81f4bfd6d26aa69dc107e15c3c4a8b44c8ae789b
Yujie-Chen <18311345153@163.com>
2025-06-05 17:17:11 +0800

    2025-06-05 Work P203-P209

---
+++ src/main/java/chapter07/HelloWorld.java
@@ -0,0 +1,17 @@
+package chapter07;
+
+/*
+*/
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		//System.out.println("Hello World!");
+		s.p("Hello World!");
+		s.p(100);
+		s.p('a');
+		s.p(100 * 200);
+		s.p(true);
+		s.p(10 / 3);
+	}
+}
+
---
+++ src/main/java/chapter07/Recursion/RecursionTest03.java
@@ -0,0 +1,51 @@
+package chapter07.Recursion;
+
+/*
+
+*/
+
+// 使用递归的方式计算n的阶乘
+// 使用for循环的方式实现一个
+public class RecursionTest03 {
+    public static void main(String[] args) {
+        int res = sum(10);
+        System.out.println(res);
+
+        recursionSum2(1, 10, 0);
+        System.out.println("----------------------------------------------------");
+
+        // 调用标准递归方法
+        int r = recursionSum(10);
+        System.out.println(r);
+
+    }
+
+    // for循环方式
+    public static int sum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    // 递归方式——自己写的
+    public static void recursionSum2(int i, int n, int sum) {
+        sum += i;
+        if (i == n) {
+            System.out.println(sum);
+            return;
+        }
+        recursionSum2(++i, n, sum);
+    }
+
+    // 递归方式
+    public static int recursionSum(int n) {
+        if (n == 1) {
+            System.out.println();
+            return 1;
+        }
+        // 程序能执行到此处说明n不是1
+        return n + recursionSum(n-1);
+    }
+}
