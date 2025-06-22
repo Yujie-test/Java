c37a79882c82329e7eba341c2f7296ba30075b51
Yujie-test <18311345153@163.com>
2025-06-22 12:45:54 +0800

    2025-06-21 Home P259-P261

---
+++ src/main/java/chapter11/ThisTest01.java
@@ -0,0 +1,20 @@
+package chapter11;
+
+/*
+	this：
+		1、this是一个关键字，全部小写
+		2、this是什么，在内存方面是怎么样的
+*/
+
+public class ThisTest01 {
+	public static void main(String[] args) {
+
+	}
+}
+
+// 顾客类
+class Customer{
+	
+}
+
+
