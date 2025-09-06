d4c85da37ae583248d326cbd0f8921e8488b04b8
Yujie-test <18311345153@163.com>
2025-09-06 11:25:39 +0800

    2025-08-31 Home P315-P316

--- src/main/java/chapter14/SuperTest07.java
+++ src/main/java/chapter14/SuperTest07.java
@@ -1,14 +1,20 @@
 package chapter14;
 
 /*
+	在父和子中有同名的属性，或者说有相同的方法
+	如果此时想在子类中访问父中的数据，必须使用“super.”加以区分
 
+	super.属性名      「访问父类的属性」
+	super.方法名(实参) 「访问父类的方法」
+	super(实参)       「调用父类的构造方法」
 */
 
 public class SuperTest07 {
 
 	// 静态方法，主方法
 	public static void main(String[] args) {
-
+		Cat c = new Cat();
+		c.yiDong();
 	}
 }
 
@@ -20,14 +26,15 @@ class Animal{
 
 class Cat extends Animal{
 	// 对move进行重写
-	public void move(){
+	/*public void move(){
 		System.out.println("Cat move!");
-	}
+	}*/
 
 	// 单独编写一个子类特有的方法
 	public void yiDong(){
-		this.move();
-		move();
-		super.move();
+		this.move(); // Animal move!
+		move(); // Animal move!
+		// “super.”不仅可以访问属性，也可以访问方法
+		super.move(); // Animal move!
 	}
 }
